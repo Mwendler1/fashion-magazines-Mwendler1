1f3a818c03ffefdfaebffbbbf87c736ca81dabcf
Matthew Wendler <102497931+Mwendler1@users.noreply.github.com>
2023-10-29 22:57:10 -0400

    Update fashion_magazines.sql

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,11 @@
--- Add your sql here
+SELECT c.customer_name, sum(unpaid.due)
+FROM customer c JOIN
+(SELECT o.customer_id,
+s.price_per_month*s.subsctription_length as due,
+o,order_status
+from orders o join subscriptions s
+on o.subscirptions_id = s.subscription-id 
+where o.order_status ="unpaid"
+and description="Fashon Magazine") unpaid
+on c.customer_id = unpaid.customer_id
+group by c.customer_name;
